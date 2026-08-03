f0469398871a63b43b7836dc5e95103ea293efe8
Michael Winsor <mgwinsor@gmail.com>
2026-08-03 23:44:12 +0800

    add: create embeddings

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -1,12 +1,81 @@
+import json
+import os
+from typing import Any, TypedDict
+
+import numpy as np
+from click import FileError
 from sentence_transformers import SentenceTransformer
+from torch.fft import Tensor
+
+
+class SemanticSearchResult(TypedDict):
+    score: float
+    title: str
+    description: str
 
 
 class SemanticSearch:
     def __init__(self, model_name: str = "all-MiniLM-L6-v2") -> None:
         self.model = SentenceTransformer(model_name)
+        self.embeddings: Tensor | None = None
+        self.documents: list[dict] | None = None
+        self.document_map: dict[int, dict] = {}
+
+    def generate_embedding(self, text: str | None) -> Tensor:
+        if not text or text.isspace():
+            raise ValueError("Cannot generate embedding for emtpy text")
+        return self.model.encode([text])[0]
+
+    def build_embeddings(self, documents: list[dict]) -> Tensor:
+        self.documents = documents
+        self.document_map = {}
+        movie_strings: list[str] = []
+        for doc in self.documents:
+            self.document_map[doc["id"]] = doc
+            movie_strings.append(f"{doc['title']}: {doc['description']}")
+        self.embeddings = self.model.encode(movie_strings, show_progress_bar=True)
+
+        os.makedirs("cache/", exist_ok=True)
+        np.save("cache/movies_embeddings.npy", self.embeddings)
+        return self.embeddings
+
+    def load_or_create_embeddings(self, documents: list[dict[str, Any]]) -> Tensor:
+        self.documents = documents
+        self.document_map = {}
+        for doc in self.documents:
+            self.document_map[doc["id"]] = doc
+
+        if os.path.exists("cache/movies_embeddings.npy"):
+            self.embeddings = np.load("cache/movies_embeddings.npy")
+            if len(self.embeddings) == len(self.documents):
+                return self.embeddings
+            else:
+                raise FileError("Stale movie embedding cache")
+        else:
+            return self.build_embeddings(self.documents)
 
 
-def verify_model():
+def verify_model() -> None:
     semantic_search = SemanticSearch()
     print(f"Model loaded: {semantic_search.model}")
     print(f"Max sequence length: {semantic_search.model.max_seq_length}")
+
+
+def verify_embeddings() -> None:
+    semantic_search = SemanticSearch()
+    with open("data/movies.json", "r") as file:
+        documents: list[dict] = json.load(file)["movies"]
+    embeddings = semantic_search.load_or_create_embeddings(documents)
+    print(type(embeddings))
+    print(f"Number of docs:   {len(documents)}")
+    print(
+        f"Embeddings shape: {embeddings.shape[0]} vectors in {embeddings.shape[1]} dimensions"
+    )
+
+
+def embed_text(text: str) -> None:
+    semantic_search = SemanticSearch()
+    embedding = semantic_search.generate_embedding(text)
+    print(f"Text: {text}")
+    print(f"First 3 dimensions: {embedding[:3]}")
+    print(f"Dimensions: {embedding.shape[0]}")
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,12 +1,10 @@
-#!/usr/bin/env python3
-
 import argparse
 import sys
 from pathlib import Path
 
 sys.path.append(str(Path(__file__).parent.parent))
 
-from cli.lib.semantic_search import verify_model
+from cli.lib.semantic_search import embed_text, verify_embeddings, verify_model
 
 
 def main() -> None:
@@ -14,12 +12,24 @@ def main() -> None:
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
     subparsers.add_parser("verify", help="Verify the embedding model is loaded")
+    subparsers.add_parser(
+        "verify_embeddings", help="Verify embeddings for the movie dataset"
+    )
+
+    single_embed_parser = subparsers.add_parser(
+        "embed_text", help="Generate an embedding for a single text"
+    )
+    single_embed_parser.add_argument("text", type=str, help="Text to embed")
 
     args = parser.parse_args()
 
     match args.command:
         case "verify":
             verify_model()
+        case "verify_embeddings":
+            verify_embeddings()
+        case "embed_text":
+            embed_text(args.text)
         case _:
             parser.print_help()
 
